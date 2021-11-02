92bd9683340ce4108fb5aecab6de1fa6350c87e5
David Fontes <doffontes@gmail.com>
2021-11-02 15:33:58 -0300

    Projeto hr-trabalhadores criado

---
+++ hr-trabalhadores/src/test/java/com/davidFontes/trabalhadores/HrTrabalhadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidFontes.trabalhadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrTrabalhadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
