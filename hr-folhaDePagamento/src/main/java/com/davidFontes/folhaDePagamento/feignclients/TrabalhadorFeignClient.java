c155e6f94523251b57e5395e4d3e3cbd0a211464
David Fontes <doffontes@gmail.com>
2021-11-03 14:51:35 -0300

    Configuracao do Load Balancer e Eureka

--- hr-folhaDePagamento/src/main/java/com/davidFontes/folhaDePagamento/feignclients/TrabalhadorFeignClient.java
+++ hr-folhaDePagamento/src/main/java/com/davidFontes/folhaDePagamento/feignclients/TrabalhadorFeignClient.java
@@ -1,15 +1,19 @@
 package com.davidFontes.folhaDePagamento.feignclients;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.davidFontes.folhaDePagamento.config.LoadBalancerConfiguration;
 import com.davidFontes.folhaDePagamento.entities.Trabalhador;
 
 @Component
-@FeignClient(name = "hr-trabalhador", url = "http://localhost:8001", path = "/trabalhadores")
+@FeignClient(name = "hr-trabalhador", path = "/trabalhadores")
+@LoadBalancerClient(name = "hr-trabalhador",
+configuration=LoadBalancerConfiguration.class)
 public interface TrabalhadorFeignClient {
 
 	@GetMapping("/{id}")
--- hr-trabalhador/src/main/java/com/davidFontes/trabalhador/resources/TrabalhadorResource.java
+++ hr-trabalhador/src/main/java/com/davidFontes/trabalhador/resources/TrabalhadorResource.java
@@ -2,6 +2,9 @@ package com.davidFontes.trabalhador.resources;
 
 import java.util.List;
 
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,11 @@ import com.davidFontes.trabalhador.services.TrabalhadoresService;
 @RestController
 @RequestMapping(value = "/trabalhadores")
 public class TrabalhadorResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(TrabalhadorResource.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	TrabalhadoresService trabalhadoresService;
@@ -27,6 +35,8 @@ public class TrabalhadorResource {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Trabalhador> buscaPorId(@PathVariable Long id) {
+
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		return ResponseEntity.ok(trabalhadoresService.buscaPorId(id));
 	}
