17b387569d63254cf6fd945df7f1a4507efd8877
David Fontes <doffontes@gmail.com>
2021-11-16 18:11:50 -0300

    hr-trabalhador como cliente do servidor de configuracao

--- hr-trabalhador/src/main/java/com/davidFontes/trabalhador/resources/TrabalhadorResource.java
+++ hr-trabalhador/src/main/java/com/davidFontes/trabalhador/resources/TrabalhadorResource.java
@@ -2,10 +2,11 @@ package com.davidFontes.trabalhador.resources;
 
 import java.util.List;
 
-import org.springframework.core.env.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,9 @@ public class TrabalhadorResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(TrabalhadorResource.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
@@ -31,6 +35,14 @@ public class TrabalhadorResource {
 	public ResponseEntity<List<Trabalhador>> buscaTodos() {
 		
 		return ResponseEntity.ok(trabalhadoresService.buscaTodos());
+	}
+	
+	@GetMapping(value = "/config")
+	public ResponseEntity<Void> buscaConfig() {
+		
+		logger.info("Configuração: " + testConfig);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping("/{id}")
