06dd7ed881b7fa6247926dc7c3643cbabef1c12a
David Fontes <doffontes@gmail.com>
2021-11-18 13:11:22 -0300

    Criando projeto Usuario, perfil e associacao ManyToMany

---
+++ hr-usuario/src/main/java/com/davidFontes/usuario/HrUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.davidFontes.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUsuarioApplication.class, args);
+	}
+
+}
---
+++ hr-usuario/src/main/java/com/davidFontes/usuario/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.davidFontes.usuario.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.davidFontes.usuario.entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+}
